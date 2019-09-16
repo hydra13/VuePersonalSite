aaa365a9587c6b6bab1be78c94810917be39cd04
hydra13 <rob.sabirov@yandex.ru>
2019-09-16 08:02:26 +0300

    Contacts - styles were fixed, Articles - mock

--- src/views/Articles.vue
+++ src/views/Articles.vue
@@ -1,15 +1,15 @@
 <template>
-<div>
-  articles
-</div>
+  <div>
+    <h5 class="card-title text-secondary">Articles</h5>
+    <p>This page is in development.</p>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'Articles'
-}
+  name: "Articles"
+};
 </script>
 
 <style>
-
 </style>
